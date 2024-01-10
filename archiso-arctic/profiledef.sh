e94170716022f770b56d22e85d448f23d30b6432
Eshan Roy (Eshanized) <148610067+eshanized@users.noreply.github.com>
2024-01-10 15:05:58 +0530

    final touch remained

--- archiso-arctic/profiledef.sh
+++ archiso-arctic/profiledef.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="archlinux"
-iso_label="ARCH_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
-iso_publisher="Arch Linux <https://archlinux.org>"
-iso_application="Arch Linux Live/Rescue CD"
-iso_version="$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
+iso_name="snigdha"
+iso_label="SNIGDHA-OS"
+iso_publisher="Snigdha <https://www.snigdhaos.org>"
+iso_application="Snigdha OS Live/Rescue CD"
+iso_version="arctic"
 install_dir="arch"
 buildmodes=('iso')
 bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
@@ -16,11 +16,12 @@ pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
 file_permissions=(
-  ["/etc/shadow"]="0:0:400"
-  ["/root"]="0:0:750"
+  ["/etc/gshadow"]="0:0:600"
+  ["/etc/shadow"]="0:0:600"
+  ["/root"]="0:0:700"
   ["/root/.automated_script.sh"]="0:0:755"
-  ["/root/.gnupg"]="0:0:700"
-  ["/usr/local/bin/choose-mirror"]="0:0:755"
-  ["/usr/local/bin/Installation_guide"]="0:0:755"
-  ["/usr/local/bin/livecd-sound"]="0:0:755"
+  ["/etc/polkit-1/rules.d"]="0:0:750"
+  ["/etc/sudoers.d"]="0:0:750"
+  ["/etc/grub.d/40_custom"]="0:0:755"
+  ["/etc/NetworkManager/dispatcher.d/09-timezone"]="0:0:755"
 )
--- build-arctic/build-arctic.sh
+++ build-arctic/build-arctic.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Author		:	Erik Dubois
 # Website		:	https://www.erikdubois.be
